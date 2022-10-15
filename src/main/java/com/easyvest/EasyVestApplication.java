31ac58fe3e9ce356761b6495029d9313acec037d
Melissa Inui <melissainui@gmail.com>
2022-10-15 17:20:55 -0300

    wip

---
+++ src/main/java/com/easyvest/EasyVestApplication.java
@@ -0,0 +1,12 @@
+package com.easyvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyVestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyVestApplication.class, args);
+	}
+}
