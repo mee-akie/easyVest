2f33b40d15bbb8cc358f417e61d1058fef76c1ce
Luiza <41277520+luizabm@users.noreply.github.com>
2022-12-10 18:01:53 -0300

    Adding the universities maps

---
+++ client/src/data/maps.js
@@ -0,0 +1,41 @@
+export const usp_map = () => {
+    return (
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1fKUzjMNQPdTn-gkTFE-watikXQ1nXBid&ehbc=2E312F"></iframe>
+    )
+}
+
+export const unicamp_map = () => {
+    return (
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1D7QrHIX-G0NPPw7z5S-bcNv-tz8y9p_E&ehbc=2E312F"></iframe>
+    )
+}
+
+export const unesp_map = () => {
+    return (
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1zvlY4vjnSFWxCjrPt1nEqptN6dkN1lna&ehbc=2E312F"></iframe>
+    )
+}
+
+export const ufabc_map = () => {
+    return (
+        <iframe src="https://www.google.com/maps/d/embed?mid=1htU7kDWtwHuqObS765PaXgtqiZnP20E&ehbc=2E312F"></iframe>
+    )
+}
+
+export const ufscar_map = () => {
+    return (
+        <iframe src="https://www.google.com/maps/d/embed?mid=1c3Q0WLLORlCpb2SUNWQ4UtGYaZrKcMo&ehbc=2E312F"></iframe>
+    )
+}
+
+export const unifesp_map = () => {
+    return (
+        <iframe src="https://www.google.com/maps/d/embed?mid=1mJTa_L5NoYESoyOmEwM9hGWQX9xsAnU&ehbc=2E312F"></iframe>
+    )
+}
+
+export const ita_map = () => {
+    return (
+        <iframe src="https://www.google.com/maps/d/embed?mid=1AXPFlSiPAefPB3HVKNNEf97EAeNdiJU&ehbc=2E312F"></iframe> 
+    )
+}
