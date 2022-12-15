9e979d0b1add8feab64b909e1f036ae5d67c7290
iwamotoraphael <iwamotoraphael@gmail.com>
2022-12-15 15:20:12 -0300

    cypress agenda tests fix

--- client/cypress/e2e/agenda_tests.cy.jsx
+++ client/cypress/e2e/agenda_tests.cy.jsx
@@ -2,6 +2,10 @@ describe('Assert agenda', () => {
 
     beforeEach(() => {
       cy.visit('http://localhost:3000/calendar')
+      cy.get('#loginUsuario').type("maria.luiza")
+      cy.get('#loginSenha').type('marialuiza123')
+      cy.get('#submitButton').click()
+      cy.visit('http://localhost:3000/calendar')
     })
 
     it('Agenda not clicked', () => {
@@ -9,10 +13,6 @@ describe('Assert agenda', () => {
     })
   
     it('Popup test after the agenda was clicked', () => {
-      cy.get('#loginUsuario').type("maria.luiza")
-      cy.get('#loginSenha').type('marialuiza123')
-      cy.get('#submitButton').click()
-      cy.visit('http://localhost:3000/calendar')
       cy.get('.calendar_default_main').click()
       cy.get('.modal_default_main').should('exist')
     })
